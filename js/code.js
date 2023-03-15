afb19528ce36cf7d5f54bb1ae23d6e0a80c4a326
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-15 11:16:18 +0000

    project done v1.0

--- js/code.js
+++ js/code.js
@@ -1,16 +1,34 @@
 window.onload = function () {
-  let shareButton = document.querySelector("a");
+  let shareButton = document.querySelector(".profile a");
   let socialMediaBox = document.querySelector(".social-media");
   let isVisible = false;
+  let screenWidth = window.innerWidth;
+
+  window.onresize = function () {
+    screenWidth = window.innerWidth;
+    console.log(screenWidth);
+  };
 
   shareButton.addEventListener("click", function (event) {
     event.preventDefault();
-    if (!isVisible) {
-      socialMediaBox.style.display = "block";
-      isVisible = !isVisible;
+    if (screenWidth > 560) {
+      if (!isVisible) {
+        socialMediaBox.style.display = "block";
+        shareButton.style.backgroundColor = "hsl(217, 19%, 35%)";
+        isVisible = !isVisible;
+      } else {
+        socialMediaBox.style.display = "none";
+        shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
+        isVisible = !isVisible;
+      }
     } else {
-      socialMediaBox.style.display = "none";
-      isVisible = !isVisible;
+      if (!isVisible) {
+        socialMediaBox.style.display = "block";
+        isVisible = !isVisible;
+      } else {
+        socialMediaBox.style.display = "none";
+        isVisible = !isVisible;
+      }
     }
   });
 };
